6792a91333c6f0994c3b74868a20db4cc63c99df
Federico Sioli <fsioli@udesa.edu.ar>
2021-03-16 11:43:57 -0300

    Prueba components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Items from "./components/items.js";
 
 function App() {
   return (
     <div className="App">
       <h1>Proyecto React N1</h1>
       <p>Tener en cuenta que todo lo que escribamos aca es el contenido</p>
+      <ul>
+        < Items /> 
+      </ul>
     </div>
   );
 }
---
+++ src/components/items.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Items() {
+    return(
+        <React.Fragment>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+        </React.Fragment>   
+    );
+}
+
+export default Items;
